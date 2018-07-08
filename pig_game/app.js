038ae5ea4cbf42a0e643247d5417714cccb4ab81
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-07-08 14:17:43 -0500

    starting the pig game

--- pig_game/app.js
+++ pig_game/app.js
@@ -9,16 +9,41 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
-
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector('#score-0').textContent;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = "none";
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  // Create a random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  //  Display dice image
+  var diceImg = document.querySelector('.dice');
+  diceImg.style.display = 'block';
+  diceImg.src = 'dice-' + dice + '.png';
+
+  // Update the round score if the rolled number is not 1
+  if(dice !== 1){
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore; 
+  }else{
+    // Current Player score == 0
+    document.querySelector('#current-' + activePlayer).textContent = '0';
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    
+  }
+});
