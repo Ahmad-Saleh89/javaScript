7c087262c8d623aa0112badd1243472c19ee707a
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-11-03 23:12:13 -0500

    Understanding reduce method

--- freeCodeCamp/scripts.js
+++ freeCodeCamp/scripts.js
@@ -41,3 +41,59 @@ const squared = (arr) => {
 }
 const squaredNumbers = squared(realNumbers);
 console.log(squaredNumbers);
+
+// The rest operator: ES6
+/* With the rest operator you can create functions that take a
+variable number of arguments. These arguments are STORED IN an ARRAY
+that can be accessed later from inside the function */
+const sum = (() => {
+  return (...args)=>{
+    return args.reduce((a,b) => a + b, 0);
+  }
+})();
+console.log(sum(1,2,3));
+
+// Understanding reduce() method
+  let numbers = [5,23,33,2];
+  function getSum(accumulator, num){
+    return accumulator + num;
+  }
+  var addIt = numbers.reduce(getSum);
+  console.log(addIt);
+  /*
+    *The reduce() method reduces the array to a SINGLE VALUE.
+    *The reduce() method REQUIRES a function to execute for each value
+    of the array (from left to right)
+    *The executed function "in this case getSum()" requires 2 parameters,
+    the first one called the accumulator, the second one represents each
+    value of the array.
+    *The first value of the accumulator will be by default the first
+    element of the array.
+  */
+//  If you want ot initiate another value you can do so:
+var addIt = numbers.reduce(getSum, 3);
+console.log(addIt);
+
+// Finding the max val using reduce()
+function findMax(accumulator, val){
+  if(val > accumulator){
+    accumulator = val;
+  }
+  return accumulator;
+}
+var maxNum = numbers.reduce(findMax);
+console.log(maxNum);
+
+// Another way to write it:
+var maxNum = numbers.reduce((accumulator, val) => {
+  if(val > accumulator){
+    accumulator = val;
+  }
+  return accumulator;
+});
+console.log(maxNum);
+
+// Third way to write it using the ternary operator:
+var maxNum = numbers.reduce((accumulator, val) => val > accumulator ? val : accumulator);
+console.log(maxNum);
+/* This line means: if (b > a) return b ,,, if not return a */
