84a44d966662c7d3f388a4916fbf61fa19fbe2a5
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-11-30 00:50:59 -0600

    More Regex

--- freeCodeCamp/script01.js
+++ freeCodeCamp/script01.js
@@ -65,3 +65,31 @@ let mixedStr = "Blueberry 3.44 are 99 delicious";
 let mixedReg = /[h-s2-6]/ig;
 let mixedResult = mixedStr.match(mixedReg);
 console.log(mixedResult);
+
+// Match Not Specified Characters using ^
+let randomStr = "3 Blind mice!@#";
+let randomReg = /[^aeiou0-9 ]/gi; // No vowels, no numbers, no spaces
+let randomResult = randomStr.match(randomReg);
+console.log(randomResult);
+
+// Match Characters that occur one or more times using +
+let city = "Mississippi is a nice city ssss";
+let sReg = /s+/ig;
+let sResult = city.match(sReg);
+console.log(sResult);
+
+// Match Characters that occur zero or more times using *
+let soccerWord = "gooooooooal";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goReg = /go*/;
+console.log(soccerWord.match(goReg));
+console.log(gPhrase.match(goReg));
+console.log(oPhrase.match(goReg));
+
+
+let text = "<h1>Winter is coming</h1>";
+let lazyRegex = /<?.h1>/g; 
+let lazyResult = text.match(lazyRegex);
+console.log(lazyResult);
+
