85e5b71445a35cd47c0ccff1ace6e7943faeb18d
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-12-04 23:59:25 -0600

    Basic Data Structure

--- freeCodeCamp/script01.js
+++ freeCodeCamp/script01.js
@@ -41,7 +41,7 @@ let gResult = ahmadStr.match(gReg);
 console.log(gResult);
 
 // Match anything with wildcard Period/Dot
-let anyStr = "let us have some fun with sun";
+let anyStr = "let us have some fun with sun un";
 let unReg = /.un/g;
 let unResult = anyStr.match(unReg);
 let funResult = unReg.test(anyStr);
@@ -87,9 +87,159 @@ console.log(soccerWord.match(goReg));
 console.log(gPhrase.match(goReg));
 console.log(oPhrase.match(goReg));
 
+// Check this out
+let titanic = "titanic tgghhhabb";
+let tiReg = /t[a-z]*a/g; // --> /t whatever a/g
+let tiResult = titanic.match(tiReg);
+console.log(tiResult);
 
+// See the difference with and without ?
 let text = "<h1>Winter is coming</h1>";
 let lazyRegex = /<?.h1>/g; 
 let lazyResult = text.match(lazyRegex);
 console.log(lazyResult);
 
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(5);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+    row = [];
+  }
+
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+let thisArray = [3,5,6];
+let thatArray = [...thisArray];
+console.log(thatArray);
+
+
+/* Only push subArrays that don't contain the elem */
+function filteredArray(arr, elem) {
+  let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i].indexOf(elem) === -1){
+       newArr.push(arr[i]);
+      }
+    }
+  return newArr;
+}
+
+// change code here to test different cases:
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 6));
+console.log(filteredArray([ ["amy", "beth", "sam"], ["dave", "sean", "peter"] ], "peter"));
+
+/*******   Basic Data Structures *********** */
+// Add Key-Value Pairs to Objects
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+console.log(foods);
+
+// Modify an Object nested within an Object
+let userActivity = {
+  id: 534346,
+  date: 'Jan 1, 2017',
+  data: {
+    totalUsers: 89,
+    online: 71,
+    onlineStatus: {
+      active: 55,
+      away: 16
+    }
+  }
+}
+userActivity.data.totalUsers = 92;
+userActivity.data.onlineStatus.active = 50;
+console.log(userActivity);
+
+// Access Property Names with Bracket Notation
+/* @@@@ Using the same foods Objects @@@@@
+ Return the price of scanned item */
+function checkInventory(scannedItem){
+  return foods[scannedItem];
+}
+console.log(checkInventory('apples'));
+
+// Remove Object properties
+delete foods.strawberries;
+
+// Check if an Object has a property
+let users = {
+  Alan: {
+    age: 25,
+    online: true
+  },
+  Jeff: {
+    age: 34,
+    online: true
+  },
+  Sarah: {
+    age: 55,
+    online: true
+  },
+  Ryan: {
+    age: 18,
+    online: true
+  }
+}
+
+function isEveryonehere(objArray, obj){
+  // for(let i = 0; i < objArray.length; i++){
+  //   if(obj.hasOwnProperty(objArray[i]) === false){
+  //     console.log('Sorry but ' + objArray[i] + ' is not among the users!');
+  //     return false;
+  //   }
+  // }
+  // console.log('All users are here! Yaaay!');
+  // return true;
+
+
+  let myBoolean = true;
+  objArray.forEach(function(user){
+    if(obj.hasOwnProperty(user) === false){
+      console.log(user + ' does not exist in the list');
+      myBoolean = false;
+    }
+  });
+  return myBoolean;
+
+}
+console.log(isEveryonehere(['hala', 'Sarah', 'Jeff', 'Ryan', 'Brittany'] , users));
+console.log(isEveryonehere(['Sarah', 'Jeff', 'Ryan', 'Alan'] , users));
+
+function isSomeuserhere(user, obj){
+  return user in obj;
+}
+console.log(isSomeuserhere('Jeff', users));
+console.log(isSomeuserhere('Ahmad', users));
+
+function countOnline(obj) {
+  let count = 0;
+for(let user in obj){
+  if(obj[user].online === true){
+   count++;
+  }
+}
+ return count;
+}
+
+console.log(countOnline(users));
