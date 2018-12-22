9fcc4b0981f315392c56022d2969173961773cd5
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-12-22 15:08:21 -0600

    IFFE

--- freeCodeCamp/script02.js
+++ freeCodeCamp/script02.js
@@ -139,3 +139,85 @@ Dog.prototype.bark = function(){
   return "I am barking";
 }
 console.log(frank.bark());
+
+// It is possible to override inherited Methods
+Dog.prototype.sayName = function(name){
+  this.name = name;
+  return "I'm the overrided name: " + name;
+}
+console.log(frank.sayName("Franky"));
+
+function Puppy(){
+
+}
+Puppy.prototype = Object.create(Dog.prototype);
+Puppy.prototype.constructor = Puppy;
+Puppy.prototype.sayName = function(name){
+  this.name = name;
+  return "I am a cute puppy: " + name;
+}
+let dedo = new Puppy();
+console.log(dedo.sayName("Dedo"));
+
+/* Use a Mixin to add common behavior between unrelated objects */
+/* Inheritance does not always work well for unrelated objects like 
+Bird and Airplane. They can both fly, but a bird is not a type of
+Airplane and vice versa */
+// A mixin allows other objects to use a collection of functions
+let myBird = {
+  name: "Donald",
+  numLegs: 2
+};
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+let glideMixin = function(obj){
+  obj.glide = function(){
+    return "My name is " + obj.name + " and I can glide on water!";
+  };
+}
+glideMixin(myBird);
+glideMixin(boat);
+
+console.log(myBird.glide());
+console.log(boat.glide());
+
+// Using Closure to protect properties
+function Car(){
+  let wheels = 4;
+  this.getWheels = function(){
+    return wheels;
+  }
+}
+let honda = new Car();
+console.log(honda.getWheels());
+
+// Immediately Invoked Function Expression (IIFE)
+(function(){
+  console.log("A cozy nest is ready");
+})();
+
+// Create a Module using IIFE
+/* IIFE is often used to group related FUNCTIONALITY into a single 
+object or Module. For example, the motion functionality */
+let motionModule = (function(){
+  return {
+    slideMixin: function(obj){
+      obj.slide = function(){
+        console.log("Yes I can slide!");
+      };
+    },
+
+    jumpMixin: function(obj){
+      obj.jump = function(){
+        console.log("Yes I can jump!");
+      };
+    }
+  }
+})();
+motionModule.slideMixin(boat);
+boat.slide();
+motionModule.jumpMixin(frank);
+frank.jump();
