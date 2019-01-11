0d99d471be26654bc1fe00b29046d03603dc75d9
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2019-01-10 20:22:30 -0600

    Eloquent_JS

---
+++ Eloquent_JS/script01.js
@@ -0,0 +1,11 @@
+// PART I
+// Chapter 1
+console.log(typeof NaN); // NaN is a number.... cool right?
+console.log(NaN == NaN); // returns false
+// Ternary Operator: ?
+console.log(true ? 1 : 2); // returns 1
+console.log(false ? 1 : 2); // returns 2
+console.log(null == undefined); // returns true
+console.log(null === undefined); // returns false
+
+// Chapter 2
