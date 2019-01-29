45ad11defe5587e975b0df55e54273efc2cd6b9b
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2019-01-28 22:58:05 -0600

    Prototype Inheritance

--- javabrains/objects.js
+++ javabrains/objects.js
@@ -18,7 +18,7 @@ console.log(bicycle1);
 bicycle2 = new Bicycle(44, 22, 4, 20);
 
 /* It'd make more sense if we ask a mechanic to inflate our tires */
-var Mechanic = function(firstName, lastName){
+function Mechanic(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
 }
@@ -35,3 +35,85 @@ console.log(bicycle2);
  * That's why we had to use the call method
  * Mike object does not have inflateTires prop itself
  */
+
+//  Protoypes *****************************/
+function greet(){
+  console.log("Hello World!");
+}
+
+console.dir(greet);
+/* Note that everytime you create a function, JS creates a prototype object refers to that function */
+console.log(greet.prototype);
+
+// Back to our Bicycle Constructor and our bicycle1 obj
+
+Bicycle.prototype.test = "This is just a test";
+console.log(bicycle1.__proto__.test);
+// Or for short
+console.log(bicycle1.test);
+
+/**
+ * There is something really important to understand
+ * if bicycle1 itself has the prop test --> it will return it
+ * if NOT it will look further in the __proto__ prop and return that value
+ * Check this out:
+ */
+bicycle1.test = "hello";
+console.log(bicycle1.test); // -> "hello"
+console.log(bicycle1.__proto__.test); // -> "This is just a test"
+
+/* If you want to know what constructor that created an obj: */
+console.log(bicycle1.constructor); // -> Bicycle
+console.log(bicycle1.__proto__.constructor); // -> also Bicycle
+
+// Prototype Inheritance
+
+function MotorCycle(speed, engine){
+  this.speed = speed;
+  this.engine = engine;
+}
+MotorCycle.prototype.startEngine = function(){
+  return "start " + this.engine;
+}
+var myMotor = new MotorCycle(240, "Yamaha");
+console.log(myMotor.startEngine());
+
+// lets develop our bicycle1 by adding a Honda engine to it
+bicycle1.engine = "Honda";
+
+bicycle1.__proto__.__proto__ = MotorCycle.prototype;
+console.log(bicycle1.startEngine());
+
+
+
+// Another Example
+function Employee(name, salary){
+  this.name = name;
+  this.salary = salary;
+}
+Employee.prototype.getName = function(){
+  console.log(this.name);
+}
+Employee.prototype.getSalary = function(){
+  console.log(this.salary);
+}
+var emp1 = new Employee("Jim", 100);
+
+
+function Manager(name, dept, salary){
+  this.name = name;
+  this.dept = dept;
+  this.salary = salary;
+}
+Manager.prototype.getDept = function(){
+  console.log(this.dept);
+}
+
+var kyle = new Manager("Kyle", "Marketing", 150);
+
+/* What if Kyle wants to get his salary?? */
+kyle.__proto__.__proto__  === Object.prototype; // true by default
+// So we can do this:
+kyle.__proto__.__proto__ = Employee.prototype;
+kyle.getName();
+kyle.getSalary(); // 150
