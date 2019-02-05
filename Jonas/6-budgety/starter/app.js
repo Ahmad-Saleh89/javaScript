4b75e70d2ed09f8c597a162f9b28fc814e93d885
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2019-02-04 23:15:36 -0600

    Adding Items to DOM

--- Jonas/6-budgety/starter/app.js
+++ Jonas/6-budgety/starter/app.js
@@ -73,7 +73,9 @@ var UIController = (function(){
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return {   //Public     // Basically we're returning an object with some methods
@@ -84,6 +86,36 @@ var UIController = (function(){
         value: document.querySelector(DOMstrings.inputValue).value
       }
     },
+    // Add a new item to the UI
+    addListItem: function(obj, type){
+      var html, container;
+      // Create HTML string with placeholder text
+      if(type === 'inc'){
+        // container is the "income__list" NOT the big container
+        container = DOMstrings.incomeContainer;
+        html = `<div class="item clearfix" id="income-${obj.id}"><div class="item__description">${obj.description}</div><div class="right clearfix"><div class="item__value">${obj.value}</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+      }else if(type === 'exp'){
+        container = DOMstrings.expensesContainer;
+        html = `<div class="item clearfix" id="expense-${obj.id}"><div class="item__description">${obj.description}</div><div class="right clearfix"><div class="item__value">${obj.value}</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`
+      }
+      // Insert the HTML into the right column in the DOM 
+      document.querySelector(container).insertAdjacentHTML('beforeend', html);
+    },
+
+    clearFields: function(){
+      var fields, fieldsArray;
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue); // --> NodeList
+      /* Note that the querySelectorAll does not return an Array, it returns a NodeList. Therefore, we can't use the array methods directly */
+      // Convert NodeList into an Array:
+      // fieldsArray = Array.prototype.slice.call(fields); --> Older JS 
+      fieldsArray = Array.from(fields); // JS 2015
+      // Loop through the new Array
+      fieldsArray.forEach(function(elem){
+        elem.value = "";
+      });
+      fieldsArray[0].focus();
+    },
+
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -112,11 +144,15 @@ var controller = (function(budgetCtrl, UICtrl){
     // 1. Get the field input data
     input = UICtrl.getInput();
     console.log(input);
-    // 2. Add the item to the budget controller
+    // 2. Add the entry to the budget controller
     newEntry = budgetCtrl.addEntry(input.type, input.description, input.value);
     // 3. Add the item to the UI
-    // 4. Calculate the budget
-    // 5. Display the budget on the UI
+    UICtrl.addListItem(newEntry, input.type);
+    // 4. Clear the fields
+    UICtrl.clearFields();
+    // 5. Calculate the budget
+
+    // 6. Display the budget on the UI
   };
 
   return {                // Public
